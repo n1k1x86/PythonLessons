b592b9dbabc561d9530d6c70ff18fc9c77449cc2
n1k1x86 <96347991+n1k1x86@users.noreply.github.com>
2023-05-03 04:58:11 +0300

    Update task_01.py

--- homework_06/tasks/task_01.py
+++ homework_06/tasks/task_01.py
@@ -8,24 +8,6 @@ Tree can only contains basic structures like:
 """
 from typing import Any
 
-# Example tree:
-example_tree = {
-    "first": ["RED", "BLUE"],
-    "second": {
-        "simple_key": ["simple", "list", "of", "RED", "valued"],
-    },
-    "third": {
-        "abc": "BLUE",
-        "jhl": "RED",
-        "complex_key": {
-            "key1": "value1",
-            "key2": "RED",
-            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
-        }
-    },
-    "fourth": "RED",
-}
-
 
 def find_occurrences(tree: dict, element: Any) -> int:
     cnt = 0
